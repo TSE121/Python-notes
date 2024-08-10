9a1c773055f55127fc48dc408b47d4c66d581e0d
TSE121 <74487245+TSE121@users.noreply.github.com>
2024-08-10 23:40:42 +0530

    file

--- OOP.py
+++ OOP.py
@@ -1,6 +1,3 @@
-from builtins import print
-
-
 class StudentInfo:
     """
     # INSTANCE VARIABLES
@@ -8,15 +5,13 @@ class StudentInfo:
 
     __init__ method This is used to create multiple instances, calling the class later on and passing down the
     arguments after self. The self statement every time creates a new instance, lets say for eg, we pass down
-    info ('Pipal Deka', 621, 7896602733, "pipal21.deka@gmail.com") as data, syntax being -
+    info ('Pipal Deka', 621, 7896607723, "pd@gmail.com") as data, syntax being -
 
-    stud_1 = StudentInfo('Pipal Deka', 621, 7896602733, "pipal21.deka@gmail.com")
+    stud_1 = StudentInfo('Pipal Deka', 621, 7896607723, "pd@gmail.com")
       |
     creates instance stud_1, passes down the info through self.
 
     this self is to be only used inside the class,
-
-
 
     # CLASS VARIABLES
         - ITS SAME FOR THE WHOLE CLASS.
@@ -24,6 +19,8 @@ class StudentInfo:
 
 
     """
+
+    class_name = "2024-B.Tech"    # Class Variable.
 
     def __init__(self, first, last, admin_no, phone, email):
         self.First = first
@@ -35,8 +32,11 @@ class StudentInfo:
     def display_name(self):
         return '{}{}'.format(self.First, self.Last)
 
+    def display_info(self):
+        return 'Name: {}{} \n'.format(self.First, self.Last), f'Class: {self.class_name}\n', f'Phone NO: {self.phone}\n'
 
-'''creating an instances into the class - stud_1 is an attribute of the class now. They are class varibles created 
+
+'''creating an instances into the class - stud_1 is an attribute of the class now. They are class variables created 
 via self, they can be used by other methods when needed without being defined again.
 '''
 
@@ -45,8 +45,4 @@ stud_1 = StudentInfo('Pipal ', 'Deka', 621, 7896602733, "pipal21.deka@gmail.com"
 # ways to call a method
 print(stud_1.display_name())  # calling directly from the instance
 print(StudentInfo.display_name(stud_1))  # calling the method from the class with passing the instance as an argument
-
-
-
-
-
+print(stud_1.display_info())
